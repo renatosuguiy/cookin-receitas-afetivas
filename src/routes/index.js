5b6c962ff2b57145208dc26658ad223659ec3a81
Renato Suguiy <renatosuguiy@gmail.com>
2021-11-10 10:46:14 -0300

    feat(Routes): Created routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,24 @@
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import Welcome from "../pages/Welcome";
 
 export const Routes = () => {
-  return <Switch></Switch>;
+  return (
+    <Switch>
+      <Route exact path='/' >
+        <Welcome />
+      </Route>
+      <Route exact path='/login' >
+        <Login />
+      </Route>
+      <Route exact path='/signup' >
+        <SignUp />
+      </Route>
+      <Route exact path='/dashboard' >
+        <Dashboard />
+      </Route>
+    </Switch>
+  );
 };
