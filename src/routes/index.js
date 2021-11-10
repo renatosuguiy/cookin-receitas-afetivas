d4ea6cb0eb5915cb311324d610e439b428360ea5
Emily <emregolao@gmail.com>
2021-11-10 13:14:23 -0300

    feat(addRecipe): structures the add new recipe pages and adds to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,21 +3,25 @@ import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import Welcome from "../pages/Welcome";
+import AddNewRecipe from "../pages/AddNewRecipe/index";
 
 export const Routes = () => {
   return (
     <Switch>
-      <Route exact path='/' >
+      <Route exact path="/">
         <Welcome />
       </Route>
-      <Route exact path='/login' >
+      <Route exact path="/login">
         <Login />
       </Route>
-      <Route exact path='/signup' >
+      <Route exact path="/signup">
         <SignUp />
       </Route>
-      <Route exact path='/dashboard' >
+      <Route exact path="/dashboard">
         <Dashboard />
+      </Route>
+      <Route exact path="/addNewRecipe">
+        <AddNewRecipe />
       </Route>
     </Switch>
   );
