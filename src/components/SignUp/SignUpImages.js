0d9741a983d20a4c80923eef4ef8c03c8b8ac9b4
Renato Suguiy <renatosuguiy@gmail.com>
2021-11-11 15:56:16 -0300

    style(SignUp): Changeg grip format

--- src/components/SignUp/SignUpImages.js
+++ src/components/SignUp/SignUpImages.js
@@ -3,12 +3,12 @@ import LogoPrimary from "../../assets/Images/logo_primary.png";
 import SignUpImage from "../../assets/Images/SignUpImage.png";
 
 const SignUpImages = () => {
-return (
-<Grid w="100%" ml="220px">
-<Image boxSize="300px" src={LogoPrimary} alt="logo" />
-<Image w="55%" h="75%" src={SignUpImage} alt="logo" />
-</Grid>
-);
+    return (
+        <Grid w="100%" ml="220px">
+            <Image boxSize="300px" src={LogoPrimary} alt="logo" />
+            <Image w="55%" h="75%" src={SignUpImage} alt="logo" />
+        </Grid>
+    );
 };
 
 export default SignUpImages;
