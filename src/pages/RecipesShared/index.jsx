5538d9b752fdcd74eca7c7f12d79716abec0cdac
Raissa Toledo <raissalst@gmail.com>
2021-11-11 15:23:56 -0300

    feat(RecipesSharedPage): adding page recipes shared

---
+++ src/pages/RecipesShared/index.jsx
@@ -0,0 +1,32 @@
+import { Box, Grid } from "@chakra-ui/layout";
+import { CardRecipes } from "../../components/Card";
+import { HeaderWelcome } from "../../components/HeaderWelcome";
+import { useSharedRecipes } from "../../providers/recipes";
+import HeaderLogo from "../../components/HeaderLogo/index";
+import Menu from "../../components/Menu";
+
+const RecipesShared = () => {
+  const { recipes } = useSharedRecipes();
+
+  return (
+    <Box>
+      <HeaderWelcome />
+      <HeaderLogo />
+      <Menu />
+      <Grid
+        w={["", "", "", "65vw"]}
+        m="0 auto"
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+        gap={10}
+        paddingX="8"
+        mt="8"
+      >
+        {recipes.map((item) => (
+          <CardRecipes key={item.id} item={item} typeCard="heart" />
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default RecipesShared;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
+import RecipesShared from "../pages/RecipesShared";
 import SignUp from "../pages/SignUp";
 import Welcome from "../pages/Welcome";
 
@@ -18,6 +19,9 @@ export const Routes = () => {
       </Route>
       <Route exact path="/dashboard">
         <Dashboard />
+      </Route>
+      <Route exact path="/recipes">
+        <RecipesShared />
       </Route>
     </Switch>
   );
