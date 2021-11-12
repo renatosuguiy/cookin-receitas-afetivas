af505d534eee904808794680e63f00d279eb2b03
Emily <emregolao@gmail.com>
2021-11-12 14:26:21 -0300

    style(arrowBack):styles the backButton

--- src/components/AddItensForm/index.js
+++ src/components/AddItensForm/index.js
@@ -6,9 +6,14 @@ import {
   Button,
   Grid,
   Heading,
+  Flex,
 } from "@chakra-ui/react";
 import { FaPlusCircle } from "react-icons/fa";
 import { useAddRecipe } from "../../providers/AddRecipe";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { HeaderWelcome } from "../HeaderWelcome/index";
+import HeaderLogo from "../HeaderLogo/index";
+import Menu from "../Menu/index";
 
 const AddItens = ({
   array,
@@ -22,79 +27,98 @@ const AddItens = ({
   const { addToArray, removeFromArray, item, setItem } = useAddRecipe();
 
   return (
-    <Container centerContent minWidth="320px">
-      <Heading size="lg" color="orange.700" m="10px auto 0px">
-        Cadastrar receita
-      </Heading>
-      <Button onClick={handleBack}>Voltar</Button>
-      <Grid m="0px" p="20px" w="100%" textAlign="center">
-        <Text fontSize="lg" m="0px auto">
-          {title}
-        </Text>
-        <Box>
-          <Input
-            placeholder="Ingrediente"
-            m="10px auto"
-            w="70%"
-            borderColor="gray"
-            onChange={(e) => setItem(e.target.value)}
-          />
-          <Button
-            colorScheme="transparent"
-            size="sm"
-            fontSize="2xl"
-            color="green.500"
-            borderRadius="25px"
-            onClick={() => addToArray(array, setArray, item)}
-          >
-            <FaPlusCircle />
-          </Button>
-        </Box>
-      </Grid>
-      <Grid
-        m="0px auto 5px"
-        p="20px"
-        w="100%"
-        textAlign="center"
-        border="1px solid gray"
-        borderRadius="25px"
-      >
-        <Text fontSize="xl" m="5px auto 10px" color="orange.700">
-          {secondaryTitle}
-        </Text>
-        {array.map((item, index) => (
-          <Box
-            key={index}
-            boxShadow="2px 2px 2px 1px rgba(0, 0, 0, 0.4)"
-            display="flex"
-            m="5px auto"
-            p="10px"
-            border="1px solid gray"
-            borderRadius="25px"
-          >
-            <Text m="0px 10px">{item}</Text>
+    <>
+      <HeaderWelcome />
+      <HeaderLogo />
+      <Menu index={1} />
+      <Container centerContent minWidth="320px">
+        <Heading size="lg" color="orange.700" m="10px auto 0px">
+          <Flex>
             <Button
-              key={index}
-              colorScheme="red"
-              size="xs"
-              fontSize="md"
-              borderRadius="20px"
-              onClick={() => removeFromArray(array, setArray, item)}
+              onClick={handleBack}
+              color="#C8561F"
+              fontSize="3xl"
+              bgColor="transparent"
+              colorScheme="none"
+              mt="8px"
+              position="relative"
+              float="left"
+              _hover={{ color: "orange.500" }}
             >
-              X
+              <FaArrowAltCircleLeft />
+            </Button>
+            <Text m="10px 35px 0px 0px">Cadastrar receita</Text>
+          </Flex>
+        </Heading>
+        <Grid m="0px" p="20px" w="100%" textAlign="center">
+          <Text fontSize="lg" m="0px auto">
+            {title}
+          </Text>
+          <Box>
+            <Input
+              placeholder="Ingrediente"
+              m="10px auto"
+              w="70%"
+              borderColor="gray"
+              onChange={(e) => setItem(e.target.value)}
+            />
+            <Button
+              colorScheme="transparent"
+              size="sm"
+              fontSize="2xl"
+              color="green.500"
+              borderRadius="25px"
+              onClick={() => addToArray(array, setArray, item)}
+            >
+              <FaPlusCircle />
             </Button>
           </Box>
-        ))}
-      </Grid>
-      <Button
-        colorScheme="orange"
-        color="white"
-        marginTop="20px"
-        onClick={handleClick}
-      >
-        {buttonText}
-      </Button>
-    </Container>
+        </Grid>
+        <Grid
+          m="0px auto 5px"
+          p="20px"
+          w="100%"
+          textAlign="center"
+          border="1px solid gray"
+          borderRadius="25px"
+        >
+          <Text fontSize="xl" m="5px auto 10px" color="orange.700">
+            {secondaryTitle}
+          </Text>
+          {array.map((item, index) => (
+            <Box
+              key={index}
+              boxShadow="2px 2px 2px 1px rgba(0, 0, 0, 0.4)"
+              display="flex"
+              m="5px auto"
+              p="10px"
+              border="1px solid gray"
+              borderRadius="25px"
+            >
+              <Text m="0px 10px">{item}</Text>
+              <Button
+                key={index}
+                colorScheme="red"
+                size="xs"
+                fontSize="md"
+                borderRadius="20px"
+                onClick={() => removeFromArray(array, setArray, item)}
+              >
+                X
+              </Button>
+            </Box>
+          ))}
+        </Grid>
+        <Button
+          colorScheme="orange"
+          color="white"
+          marginTop="20px"
+          onClick={handleClick}
+        >
+          {buttonText}
+        </Button>
+      </Container>
+    </>
   );
 };
 
--- src/pages/AddNewRecipe/page_1.js
+++ src/pages/AddNewRecipe/page_1.js
@@ -9,9 +9,14 @@ import {
   Button,
   Grid,
   Heading,
+  Flex,
 } from "@chakra-ui/react";
 import { useHistory } from "react-router";
 import { useAddRecipe } from "../../providers/AddRecipe";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import HeaderLogo from "../../components/HeaderLogo";
+import Menu from "../../components/Menu";
+import { HeaderWelcome } from "../../components/HeaderWelcome/index";
 
 const NewRecipePage01 = () => {
   const history = useHistory();
@@ -24,58 +29,78 @@ const NewRecipePage01 = () => {
   };
 
   return (
-    <Container centerContent minWidth="320px">
-      <Heading size="lg" color="orange.700" m="10px auto 0px">
-        Cadastrar receita
-      </Heading>
-      <Box width="90%" maxWidth="450px">
-        <Image src={ChefImage} alt="Chef" w="70%" m="0px auto" />
-      </Box>
-      <Grid
-        as="form"
-        border="1px solid gray"
-        borderRadius="25px"
-        m="20px"
-        p="20px"
-        w="90%"
-        textAlign="center"
-      >
-        <Text fontSize="md" m="10px auto">
-          Digite o nome da sua receita
-        </Text>
-        <Input
-          placeholder="&nbsp;Nome da receita"
-          m="10px auto"
-          w="80%"
-          onChange={(e) =>
-            setRecipeBody({ ...recipeBody, title: e.target.value })
-          }
-        />
-        <Text fontSize="md">Selecione a categoria da sua receita</Text>
-        <Select
-          placeholder="&nbsp;Categoria da sua receita"
-          variant="outline"
-          w="80%"
-          m="10px auto"
-          borderColor="gray"
-          onChange={(e) => {
-            setRecipeBody({ ...recipeBody, category: e.target.value });
-          }}
+    <>
+      <HeaderWelcome />
+      <HeaderLogo />
+      <Menu index={1} />
+      <Container centerContent minWidth="320px">
+        <Heading size="lg" color="orange.700" m="10px auto 0px">
+          <Flex>
+            <Button
+              onClick={() => history.push("/dashboard")}
+              color="#C8561F"
+              fontSize="3xl"
+              bgColor="transparent"
+              colorScheme="none"
+              mt="8px"
+              position="relative"
+              float="left"
+              _hover={{ color: "orange.500" }}
+            >
+              <FaArrowAltCircleLeft />
+            </Button>
+            <Text m="10px 35px 0px 0px">Cadastrar receita</Text>
+          </Flex>
+        </Heading>
+        <Box width="90%" maxWidth="450px">
+          <Image src={ChefImage} alt="Chef" w="70%" m="0px auto" />
+        </Box>
+        <Grid
+          as="form"
+          border="1px solid gray"
+          borderRadius="25px"
+          m="20px"
+          p="20px"
+          w="90%"
+          textAlign="center"
         >
-          <option value="salgado">Salgado</option>
-          <option value="doce">Doce</option>
-          <option value="bebida">Bebida</option>
-        </Select>
-      </Grid>
-      <Button
-        colorScheme="orange"
-        color="white"
-        marginTop="20px"
-        onClick={nextPage}
-      >
-        Prosseguir
-      </Button>
-    </Container>
+          <Text fontSize="md" m="10px auto">
+            Digite o nome da sua receita
+          </Text>
+          <Input
+            placeholder="&nbsp;Nome da receita"
+            m="10px auto"
+            w="80%"
+            onChange={(e) =>
+              setRecipeBody({ ...recipeBody, title: e.target.value })
+            }
+          />
+          <Text fontSize="md">Selecione a categoria da sua receita</Text>
+          <Select
+            placeholder="&nbsp;Categoria da sua receita"
+            variant="outline"
+            w="80%"
+            m="10px auto"
+            borderColor="gray"
+            onChange={(e) => {
+              setRecipeBody({ ...recipeBody, category: e.target.value });
+            }}
+          >
+            <option value="salgado">Salgado</option>
+            <option value="doce">Doce</option>
+            <option value="bebida">Bebida</option>
+          </Select>
+        </Grid>
+        <Button
+          colorScheme="orange"
+          color="white"
+          marginTop="20px"
+          onClick={nextPage}
+        >
+          Prosseguir
+        </Button>
+      </Container>
+    </>
   );
 };
 
