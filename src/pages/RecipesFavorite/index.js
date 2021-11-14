8d427bd329256245ed7644019796bcf5c21f0f40
Manoela Cunha <manoela_fg@hotmail.com>
2021-11-14 14:04:22 -0300

    feat(pages): add empty page condition to RecipesFavorite page

--- src/pages/RecipesFavorite/index.js
+++ src/pages/RecipesFavorite/index.js
@@ -5,27 +5,43 @@ import HeaderLogo from "../../components/HeaderLogo/index";
 import { SearchBox } from "../../components/SearchBox";
 import Menu from "../../components/Menu";
 import { CardsList } from "../../components/CardsList";
+import { useEffect } from "react";
 
 const RecipesFavorite = () => {
   const {
+    recipes,
     recipeFavorites,
     recipesSharedFound,
     setRecipesSharedFound,
     searchForRecipePublic,
+    getFavoriteRecipes,
   } = useSharedRecipes();
+
+  const user = localStorage.getItem("@cookin:user") || "";
+  const userId = JSON.parse(user).id;
+
+  useEffect(() => {
+    getFavoriteRecipes(userId);
+  }, [recipes]);
 
   return (
     <Box>
       <HeaderWelcome />
       <HeaderLogo />
       <Menu />
-      <SearchBox functionToSearch={searchForRecipePublic} />
-      <CardsList
-        state={recipeFavorites}
-        stateOfSearchedRecipes={recipesSharedFound}
-        setStateOfSearchedRecipes={setRecipesSharedFound}
-        typeCard="heart"
-      />
+      {recipeFavorites.length === 0 ? (
+        <p>Página Vazia</p> //Vou adicionar o componente EmptyFavoritesPage
+      ) : (
+        <>
+          <SearchBox functionToSearch={searchForRecipePublic} />
+          <CardsList
+            state={recipeFavorites}
+            stateOfSearchedRecipes={recipesSharedFound}
+            setStateOfSearchedRecipes={setRecipesSharedFound}
+            typeCard="heart"
+          />
+        </>
+      )}
     </Box>
   );
 };
