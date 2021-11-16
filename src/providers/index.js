b7be6943f472c7cabf49e357a40082b799dacce6
Raissa Toledo <raissalst@gmail.com>
2021-11-16 14:01:21 -0300

    feat(providerRecipes): adding toasts to recipes provider, embracing all other providers with chakraprovider at index.js of providers, changing Card function to delete public/private recipe and calling functions from recipes provider at Recipe Details page

--- src/providers/index.js
+++ src/providers/index.js
@@ -7,14 +7,14 @@ import { AddRecipeProvider } from "./AddRecipe";
 
 export const AppProvider = ({ children }) => {
   return (
-    <RecipesProvider>
-      <AuthProvider>
-        <MyRecipesProvider>
-          <AddRecipeProvider>
-            <ChakraProvider theme={theme}>{children}</ChakraProvider>
-          </AddRecipeProvider>
-        </MyRecipesProvider>
-      </AuthProvider>
-    </RecipesProvider>
+    <ChakraProvider theme={theme}>
+      <RecipesProvider>
+        <AuthProvider>
+          <MyRecipesProvider>
+            <AddRecipeProvider>{children}</AddRecipeProvider>
+          </MyRecipesProvider>
+        </AuthProvider>
+      </RecipesProvider>
+    </ChakraProvider>
   );
 };
