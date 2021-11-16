f3384d541fea0e211bf2de742c79e64b80a346c8
Manoela Cunha <manoela_fg@hotmail.com>
2021-11-16 00:03:37 -0300

    style(pages): add media query to RecipesPrivate page

--- src/pages/RecipesPrivate/index.js
+++ src/pages/RecipesPrivate/index.js
@@ -1,13 +1,17 @@
 import { Box } from "@chakra-ui/layout";
-import { Button } from "@chakra-ui/react";
-import { HeaderWelcome } from "../../components/HeaderWelcome";
-import { useMyRecipes } from "../../providers/MyRecipes";
-import HeaderLogo from "../../components/HeaderLogo/index";
-import { SearchBox } from "../../components/SearchBox";
+import { AddIcon } from "@chakra-ui/icons";
+import { Button, IconButton } from "@chakra-ui/react";
+
 import Menu from "../../components/Menu";
-import { CardsList } from "../../components/CardsList";
-import { useHistory } from "react-router";
+import HeaderLogo from "../../components/HeaderLogo/index";
 import EmptyPage from "../../components/EmptyPage";
+import { HeaderWelcome } from "../../components/HeaderWelcome";
+import { SearchBox } from "../../components/SearchBox";
+import { CardsList } from "../../components/CardsList";
+
+import { useHistory } from "react-router";
+import { useMediaQuery } from "@mui/material";
+import { useMyRecipes } from "../../providers/MyRecipes";
 
 const RecipesPrivate = () => {
   const {
@@ -18,6 +22,8 @@ const RecipesPrivate = () => {
   } = useMyRecipes();
 
   const history = useHistory();
+
+  const isLagerThan480 = useMediaQuery("(min-width: 480px)");
 
   return (
     <Box>
@@ -35,18 +41,32 @@ const RecipesPrivate = () => {
             setStateOfSearchedRecipes={setRecipesPrivateFound}
             typeCard="times"
           />
-          <Box w="250px" margin="0 auto">
-            <Button
-              bg="orange.200"
-              h="54px"
-              w="250px"
-              color="#ffffff"
-              mt="6"
+          {isLagerThan480 ? (
+            <Box w="250px" margin="0 auto">
+              <Button
+                bg="orange.200"
+                h="54px"
+                w="250px"
+                color="white.50"
+                mt="6"
+                onClick={() => history.push("/addRecipe")}
+              >
+                Cadastrar receita
+              </Button>
+            </Box>
+          ) : (
+            <IconButton
+              size="lg"
+              color="white.50"
+              backgroundColor="orange.700"
+              borderRadius="100%"
+              position="fixed"
+              bottom="10px"
+              left="10px"
+              icon={<AddIcon />}
               onClick={() => history.push("/addRecipe")}
-            >
-              Cadastrar receita
-            </Button>
-          </Box>
+            />
+          )}
         </>
       )}
     </Box>
